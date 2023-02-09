3e8882deb234df1387d83a8940dfee01fd1b5a1b
renel34 <laplanter96@gmail.com>
2023-02-09 17:36:51 -0500

    updated the code

--- Practice.py
+++ Practice.py
@@ -2,14 +2,27 @@ def create_strings_from_characters(frequencies, string1, string2):
     number = 2
     dict1 = {}
     dict2 = {}
-    for letter in string1:
+    dict3 = {}
+    for letter in frequencies:
         dict1[letter] = dict1.get(letter,0) + 1
-    for letter in string2:
         dict2[letter] = dict2.get(letter,0) + 1
-    
+        dict3[letter] = dict3.get(letter,0) + 1
+    for letter in string1:
+        dict1[letter] = dict1.get(letter,0) - 1
+        if dict1[letter] < 0:
+            number -= 1
+    for letter in string2:
+        dict2[letter] = dict2.get(letter,0) - 1
+        if dict2[letter] < 0:
+            number -= 1
+    for letter in string1 + string2:
+        dict3[letter] = dict3.get(letter,0) - 1
 
-
-    
+    print(dict1)
+    print(dict2)
+    print(dict3)
+    print(number)
+            
     
 frequencies = {"h": 2, "e": 1, "l": 1, "r": 4, "a": 3, "o": 2, "d": 1, "w": 1}
 string1 = "hello"

5deb50b8bbabede48ff753b8f083aadb77aae687
renel34 <laplanter96@gmail.com>
2023-02-25 18:30:01 -0500

    Final and sucessful code.

--- Properties.py
+++ Properties.py
@@ -4,14 +4,13 @@ class BankAccount:
         self._balance = 0
 
     def set_balance(self, balance):
-        if balance is not int:
-            if balance < 0 or balance > 100000:
-                raise ValueError("Enter number between 0 and 100,000!")
-            self._balance = balance
+        if type(balance) == int or type(balance) == float:
+            if balance > 0 and balance < 100000:
+                self._balance = balance
     
     def get_balance(self):
         return round(self._balance)
     
 account = BankAccount("Clement")
-account.set_balance(100001)
+account.set_balance(56.6)
 print(account.get_balance())

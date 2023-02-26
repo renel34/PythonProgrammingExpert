e04a197ef4db5863aff6d86dfbd7ee8f4ab0c84e
renel34 <laplanter96@gmail.com>
2023-02-25 20:51:14 -0500

    code for question no 5

---
+++ Class Methods and Attributes.py
@@ -0,0 +1,14 @@
+class Employee:
+    average_age = 0
+    average_salary = 0
+
+    def __init__name(self, name, age, salary):
+        self.name = name
+        self.age = age
+        self.salary = salary
+        Employee.average_age += self.age
+        Employee.average_salary += self.salary
+
+empl1 = Employee("Rene", 60, 58000)
+empl2 = Employee("Luc", 58, 40000)
+print(empl1.age)

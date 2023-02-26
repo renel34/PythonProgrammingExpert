6f80c862d9cd48dad7abda83ced101e34e6b1b21
renel34 <laplanter96@gmail.com>
2023-02-25 19:49:45 -0500

    Final code for question 5

--- Properties.py
+++ Properties.py
@@ -3,14 +3,17 @@ class BankAccount:
         self.account_holder_name = account_holder_name
         self._balance = 0
 
-    def set_balance(self, balance):
+    @property
+    def balance(self):
+        return round(self._balance)
+
+    @balance.setter
+    def balance(self, balance):
         if type(balance) == int or type(balance) == float:
             if balance > 0 and balance < 100000:
                 self._balance = balance
     
-    def get_balance(self):
-        return round(self._balance)
-    
+        
 account = BankAccount("Clement")
-account.set_balance(56.6)
-print(account.get_balance())
+account.balance = 99999
+print(account.balance)

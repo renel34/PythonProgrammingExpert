f59d772b6e3976e89ce535c3b6b973d8c8d89f5c
renel34 <laplanter96@gmail.com>
2023-02-28 10:04:42 -0500

    First code for question 5

---
+++ ClassMethodsAndAttributes.py
@@ -0,0 +1,8 @@
+class Employee:
+    average_age = 0
+    average_salary = 0
+    
+    def __ini__(self, name, age, salary):
+        self.name = name
+        self.age = age
+        self.salary = salary

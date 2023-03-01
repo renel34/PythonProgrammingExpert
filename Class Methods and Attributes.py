73b4e5e8ab950aae6dd8905dc0b5a0d8e69ed490
renel34 <laplanter96@gmail.com>
2023-03-01 11:13:07 -0500

    Final code for question 5

--- Class Methods and Attributes.py
+++ Class Methods and Attributes.py
@@ -2,15 +2,20 @@ class Employee:
     average_age = 0
     average_salary = 0
     nbr_employee = 0
+    employee_age = 0
+    employee_salary = 0
 
     def __init__(self, name, age, salary):
         self.name = name
         self.age = age
         self.salary = salary
         Employee.nbr_employee += 1
-        Employee.average_age += self.age
-        Employee.average_salary += self.salary
+        Employee.employee_age += self.age
+        Employee.employee_salary += self.salary
+        Employee.average_age = Employee.employee_age/Employee.nbr_employee
+        Employee.average_salary = Employee.employee_salary/Employee.nbr_employee
 
+        
 empl1 = Employee("Rene", 60, 58000)
-empl2 = Employee("Luc", 58, 40000)
+#empl2 = Employee("Luc", 58, 40000)
 print(Employee.average_age)

99e3fefaeaa8bb93da0e3685d01c9132cd305549
renel34 <laplanter96@gmail.com>
2023-03-01 12:55:22 -0500

    first code for question 6

--- Class Methods and Attributes.py
+++ Class Methods and Attributes.py
@@ -1,21 +1,19 @@
-class Employee:
-    average_age = 0
-    average_salary = 0
-    nbr_employee = 0
-    employee_age = 0
-    employee_salary = 0
+class Temperature:
+    min_temperature = 0
+    max_temperature = 1000
 
-    def __init__(self, name, age, salary):
-        self.name = name
-        self.age = age
-        self.salary = salary
-        Employee.nbr_employee += 1
-        Employee.employee_age += self.age
-        Employee.employee_salary += self.salary
-        Employee.average_age = Employee.employee_age/Employee.nbr_employee
-        Employee.average_salary = Employee.employee_salary/Employee.nbr_employee
+    def __ini__(self, kelvin):
+        self.kelvin = kelvin
+    
+    @classmethod
+    def update_min_temperature(cls, kelvin):
+        if Temperature.min_temperature < Temperature.max_temperature:
+            Temperature.min_temperature = kelvin
 
-        
-empl1 = Employee("Rene", 60, 58000)
-#empl2 = Employee("Luc", 58, 40000)
-print(Employee.average_age)
+    @classmethod
+    def update_max_temperature(cls, kelvin):
+        Temperature.max_temperature = kelvin
+
+t1 = Temperature(260)
+Temperature.update_max_temperature(270)
+Temperature.update_min_temperature(680)

280e3fd913812c6ceddb28ee5a5b0845681c07fe
renel34 <laplanter96@gmail.com>
2023-03-10 22:51:12 -0500

    latest update on question 6

--- Class Methods and Attributes.py
+++ Class Methods and Attributes.py
@@ -5,26 +5,26 @@ class Temperature:
 
     def __init__(self, kelvin):
         self.kelvin = kelvin
+        print(f"min = {self.min_temperature} max = {self.max_temperature}")
+        if self.kelvin < Temperature.min_temperature or self.kelvin > Temperature.max_temperature:
+            raise ValueError("Invalid Temperature.")
         
     @classmethod
     def update_min_temperature(cls, kelvin):
-        print(f"min_temperature = {cls.min_temperature} and max_temperature = {cls.max_temperature}")
-        print(kelvin)
-        if kelvin > cls.min_temperature and kelvin < cls.max_temperature:
-            if kelvin < cls.max_temperature:
-                cls.min_temperature = kelvin
-        raise Exception("Invalid temperature.")
-           
+        cls.min_temperature = kelvin
+        print(f"min = {cls.min_temperature} max = {cls.max_temperature}")
+        if kelvin > cls.max_temperature:
+            raise Exception("Invalid temperature.")
+        print(f"min = {cls.min_temperature} max={cls.max_temperature}")
+            
 
     @classmethod
     def update_max_temperature(cls, kelvin):
-        #print(f"min = {cls.min_temperature}  max = {cls.max_temperature}")
-        if kelvin > cls.min_temperature and kelvin < cls.max_temperature:
-            #print(F"{kelvin} is between {cls.min_temperature} and {cls.max_temperature}")
-            cls.max_temperature = kelvin
-            #print(f"min = {cls.min_temperature}  max = {cls.max_temperature}")
+        cls.max_temperature = kelvin
+        print(f"min = {cls.min_temperature} max = {cls.max_temperature}")
         
 
 t1 = Temperature(260)
 Temperature.update_max_temperature(270)
 Temperature.update_min_temperature(680)
+t2 = Temperature(280)

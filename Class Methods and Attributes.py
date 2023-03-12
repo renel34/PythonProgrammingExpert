a7cf294653bd2e73a0663b8f768893261586501f
renel34 <laplanter96@gmail.com>
2023-03-12 07:10:25 -0400

    Final code for question 6

--- Class Methods and Attributes.py
+++ Class Methods and Attributes.py
@@ -7,18 +7,28 @@ class Temperature:
             raise Exception("Invalid temperature.")
         self.kelvin = kelvin
     
+
     @classmethod
     def update_min_temperature(cls, kelvin):
-        if cls.kelvin < cls.max_temperature:
-            Temperature.min_temperature = cls.kelvin
-        raise Exception("Invalid temperature.")
+        if kelvin > cls.max_temperature:
+            raise Exception("Invalid temperature.")
+        cls.min_temperature = kelvin
+
 
     @classmethod
     def update_max_temperature(cls, kelvin):
-        cls.max_temperature = cls.kelvin
+        if kelvin < cls.min_temperature:
+            raise Exception("Invalid temperature.")
+        cls.max_temperature = kelvin
 
 
 t1 = Temperature(260)
+Temperature.update_max_temperature(270)
+Temperature.update_min_temperature(680)
+t2 = Temperature(280)
+Temperature.update_min_temperature(100)
+
+
 
 
 

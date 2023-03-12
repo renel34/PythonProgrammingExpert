0c1c54cb156556b00d23d78576778a7695cd00c7
renel34 <laplanter96@gmail.com>
2023-03-12 07:00:27 -0400

    latest update on question 6

--- Class Methods and Attributes.py
+++ Class Methods and Attributes.py
@@ -1,30 +1,25 @@
 class Temperature:
     min_temperature = 0
     max_temperature = 1000
-    kelvin = 0
-
+    
     def __init__(self, kelvin):
+        if kelvin < self.min_temperature or kelvin > self.max_temperature:
+            raise Exception("Invalid temperature.")
         self.kelvin = kelvin
-        print(f"min = {self.min_temperature} max = {self.max_temperature}")
-        if self.kelvin < Temperature.min_temperature or self.kelvin > Temperature.max_temperature:
-            raise ValueError("Invalid Temperature.")
-        
+    
     @classmethod
     def update_min_temperature(cls, kelvin):
-        cls.min_temperature = kelvin
-        print(f"min = {cls.min_temperature} max = {cls.max_temperature}")
-        if kelvin > cls.max_temperature:
-            raise Exception("Invalid temperature.")
-        print(f"min = {cls.min_temperature} max={cls.max_temperature}")
-            
+        if cls.kelvin < cls.max_temperature:
+            Temperature.min_temperature = cls.kelvin
+        raise Exception("Invalid temperature.")
 
     @classmethod
     def update_max_temperature(cls, kelvin):
-        cls.max_temperature = kelvin
-        print(f"min = {cls.min_temperature} max = {cls.max_temperature}")
-        
+        cls.max_temperature = cls.kelvin
+
 
 t1 = Temperature(260)
-Temperature.update_max_temperature(270)
-Temperature.update_min_temperature(680)
-t2 = Temperature(280)
+
+
+
+    
